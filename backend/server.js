caeebe896958b9a4fb540dc034f5098954ddd73b
Brad Traversy <bradtraversy@local.local>
2020-09-23 15:52:18 -0400

    Serve prooducts from backend

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
+
+app.listen(5000, console.log('Server running on port 5000'))
