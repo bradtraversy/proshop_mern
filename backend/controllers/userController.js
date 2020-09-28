dee54ebe350227b1d74d0d49f42c25f73371bf7e
Brad Traversy <bradtraversy@local.local>
2020-09-28 13:09:20 -0400

    Fixed isAdmin update bug

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -150,7 +150,7 @@ const updateUser = asyncHandler(async (req, res) => {
   if (user) {
     user.name = req.body.name || user.name
     user.email = req.body.email || user.email
-    user.isAdmin = req.body.isAdmin || user.isAdmin
+    user.isAdmin = req.body.isAdmin
 
     const updatedUser = await user.save()
 
