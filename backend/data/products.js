2cdb39c89e814d20fce66d765a3f04c59d31abba
Brad Traversy <bradtraversy@local.local>
2020-09-30 09:30:31 -0400

    Changed sample product reviews to 0

--- backend/data/products.js
+++ backend/data/products.js
@@ -8,8 +8,8 @@ const products = [
     category: 'Electronics',
     price: 89.99,
     countInStock: 3,
-    rating: 4.5,
-    numReviews: 4,
+    rating: 0,
+    numReviews: 0,
   },
   {
     name: 'iPhone 11 Pro 256GB Memory',
@@ -20,8 +20,8 @@ const products = [
     category: 'Electronics',
     price: 599.99,
     countInStock: 10,
-    rating: 4.0,
-    numReviews: 4,
+    rating: 0,
+    numReviews: 0,
   },
   {
     name: 'Cannon EOS 80D DSLR Camera',
@@ -31,9 +31,9 @@ const products = [
     brand: 'Cannon',
     category: 'Electronics',
     price: 929.99,
-    countInStock: 5,
-    rating: 3,
-    numReviews: 3,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
   },
   {
     name: 'Sony Playstation 4 Pro White Version',
@@ -44,8 +44,8 @@ const products = [
     category: 'Electronics',
     price: 399.99,
     countInStock: 10,
-    rating: 5,
-    numReviews: 3,
+    rating: 0,
+    numReviews: 0,
   },
   {
     name: 'Logitech G-Series Gaming Mouse',
@@ -56,8 +56,8 @@ const products = [
     category: 'Electronics',
     price: 49.99,
     countInStock: 7,
-    rating: 3.5,
-    numReviews: 2,
+    rating: 0,
+    numReviews: 0,
   },
   {
     name: 'Amazon Echo Dot 3rd Generation',
@@ -68,8 +68,8 @@ const products = [
     category: 'Electronics',
     price: 29.99,
     countInStock: 0,
-    rating: 4,
-    numReviews: 4,
+    rating: 0,
+    numReviews: 0,
   },
 ]
 
