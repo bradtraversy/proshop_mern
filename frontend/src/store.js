6c8c715bf4167b360a27dbd88ae25dcfefdaf49f
Dian Rahmaji <dianrahmaji@gmail.com>
2020-10-06 13:12:25 +0700

    add paymentMethodFromStorage in store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -65,10 +65,15 @@ const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
   ? JSON.parse(localStorage.getItem('shippingAddress'))
   : {}
 
+const paymentMethodFromStorage = localStorage.getItem('paymentMethod')
+  ? JSON.parse(localStorage.getItem('paymentMethod'))
+  : {}
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
     shippingAddress: shippingAddressFromStorage,
+    paymentMethod: paymentMethodFromStorage,
   },
   userLogin: { userInfo: userInfoFromStorage },
 }
