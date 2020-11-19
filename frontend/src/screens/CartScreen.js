eddf317da03b49d3c08bd98d677a418276b06452
Tarun Soni <33228915+tarun-soni@users.noreply.github.com>
2020-11-19 14:45:49 +0530

    wrapped item.qty with Number() avoiding string addition issue
    sometimes subtotal adds strings instead of numbers for eg '4' + '6' which should be 10 subtotal, it displays 46. wrapping item.qty in Number() fixes this.

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -86,7 +86,7 @@ const CartScreen = ({ match, location, history }) => {
           <ListGroup variant='flush'>
             <ListGroup.Item>
               <h2>
-                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                Subtotal ({cartItems.reduce((acc, item) => acc + Number(item.qty), 0)})
                 items
               </h2>
               $
