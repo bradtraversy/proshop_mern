b91461bff8b20f444b0fd2af4816ddf2948fa694
talmax1124 <carlitosdiazplaza@gmail.com>
2021-03-20 18:53:07 -0400

    l

--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  })
-}
-
-export default generateToken
