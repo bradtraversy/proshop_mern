a5b0780fb068c1326a05984e1a6f9dff761de9c7
Zayed Ibn Ibrahim <51837536+zayedibnibrahim@users.noreply.github.com>
2021-06-27 00:01:07 +0600

    Reducer for reset product details
    Reducer for PRODUCT_DETAILS_RESET

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -54,6 +54,8 @@ export const productDetailsReducer = (
       return { loading: false, product: action.payload }
     case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload }
+    case PRODUCT_DETAILS_RESET:
+      return { product: { reviews: [] } }
     default:
       return state
   }
