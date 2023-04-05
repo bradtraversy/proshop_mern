899305d91e59d52f94565b137fd9ef1308e8177a
hollyxj <hmj19a@fsu.edu>
2023-04-05 14:25:01 -0400

    Removed unused import of 'mongoose' and 'colors

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,6 +1,4 @@
-import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import colors from 'colors'
 import users from './data/users.js'
 import products from './data/products.js'
 import User from './models/userModel.js'
