0026ba272873f4f597be2db64b61669a528f498c
tylerwelsh <tylerwelsh@live.com>
2023-04-05 14:39:26 -0400

    Removed unused Import
    Code smell #1 for me is fixed.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
-import colors from 'colors'
 import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
