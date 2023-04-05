98a0adbe9661f35c4101198b6e841d50b514274c
tylerwelsh <tylerwelsh@live.com>
2023-04-05 14:49:59 -0400

    Removed unnecessary return statement
    The "return" is not needed.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -18,7 +18,6 @@ const addOrderItems = asyncHandler(async (req, res) => {
   if (orderItems && orderItems.length === 0) {
     res.status(400)
     throw new Error('No order items')
-    return
   } else {
     const order = new Order({
       orderItems,
