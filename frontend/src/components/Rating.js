1a2b236da6229b7cc26f8a14453005442f0e9675
tylerwelsh <tylerwelsh@live.com>
2023-04-07 11:11:34 -0400

    Update Rating.js
    This code smell was changed for better readability. What is happening is if text is truthful (meaning it is not null, undefined, 0, etc) then we render what the text prop actually is. Using the ternary operator we are able to read it much better as "If text is true and not null, then render text, otherwise, render nothing."

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -63,7 +63,7 @@ const Rating = ({ value, text, color }) => {
           }
         ></i>
       </span>
-      <span>{text && text}</span>
+      <span>{text ? text : null}</span>
     </div>
   )
 }
