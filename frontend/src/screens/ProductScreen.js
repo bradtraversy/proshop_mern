dc0cd49425b7308b7251eff6c33b7287a97bcf08
Mariela Badillo <mlbadillo8@gmail.com>
2023-04-08 16:01:47 -0400

    correctly extracted ternary operation from ProductScreen.js

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -57,39 +57,50 @@ const ProductScreen = ({ history, match }) => {
     )
   }
 
-  return (
+  
+  if(loading) return(
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+          Go Back
+      </Link>
+      <Loader/>
+    </>
+  ) 
+  if(error) return(
     <>
       <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
-
-      if(loading) return <Loader/>
-
-      if(error) return <Message variant='danger'>{error}</Message>
-
-      return (
-        <>
-          <Meta title={product.name} />
-          <Row>
-            <Col md={6}>
-              <Image src={product.image} alt={product.name} fluid />
-            </Col>
-            <Col md={3}>
-              <ListGroup variant='flush'>
-                <ListGroup.Item>
-                  <h3>{product.name}</h3>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Rating
-                    value={product.rating}
-                    text={`${product.numReviews} reviews`}
-                  />
-                </ListGroup.Item>
-                <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-                <ListGroup.Item>
-                  Description: {product.description}
-                </ListGroup.Item>
-              </ListGroup>
+      <Message variant='danger'>{error}</Message>
+    </>
+  )
+      
+  return(
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      <Meta title={product.name} />
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+              </ListGroup.Item>
+              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+              <ListGroup.Item>
+                Description: {product.description}
+              </ListGroup.Item>
+            </ListGroup>
             </Col>
             <Col md={3}>
               <Card>
@@ -218,8 +229,6 @@ const ProductScreen = ({ history, match }) => {
           </Row>
         </>
       )
-    </>
-  )
 }
 
 export default ProductScreen
