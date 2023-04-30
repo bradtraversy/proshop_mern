b8a78288c54c54edc1dc235065c62bb236784248
Steven Matiz <william.s.matiz@gmail.com>
2023-04-29 23:27:18 -0400

    Postfactor with comments explaining what I did + Set up stable coding environment As mentioned in the email to the Professor and TA, on 4/28/2023 I coded about 35% of feature 2 on Brian's apre linux laptop on my branch thinking I was signed into my git account. This is because my Windows computer is currently broken with WSL (path variables are destroyed and deleting/re-installing is not working). So Brian let me used his spare linux laptop and later that night we realized after he went home that it pushed under his account.
    Fast foward to today, this morning I tried getting my commits in properly by copy/pasting the commits done in his account on the Windows computer, but this proved rather difficult as again, the project can't run on there. I made those commits around 3pm. That's when I decided to go to Best Buy to buy a MacBook with intents to return it after.
    
    This leads to this specific commit. After purchasing the MacBook, re-buying Brad's Umedy course videos to re-learn how to set up from scratch, and getting a lot of help from my team mates this time as all of them have Macs and knew how to troubleshoot compared to when I was using my Windows computer, I was finally able to re set up the project just now. In this commit, I made comments explaining the 35% of feature 2 I coded on 4/28/2023 just so the GitHub can show my name and a proper git commit with working code.
    
    Thank you in particular for your understanding and for Brian helping me the most when it came to setting up the mac book.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -17,7 +17,7 @@ import { useState, useEffect } from 'react';
 
 const HomeScreen = ({ match }) => {
 
-  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState(''); //stores the currently selected category in the drop down list 
   const keyword = match.params.keyword;
 
   const pageNumber = match.params.pageNumber || 1;
@@ -43,11 +43,12 @@ const HomeScreen = ({ match }) => {
   if (error) return <Message variant="danger">{error}</Message>;
 
   const handleChange = (event) =>{
-    setSelectedCategory(event.target.value) //helps for drop down stuff
+    setSelectedCategory(event.target.value) //helps for drop down selection. this updates the selectedCategory state with the new value selected from user
   }
 
 
-  const categories = [...new Set(products.map((product) => product.category))];
+  const categories = [...new Set(products.map((product) => product.category))]; {/* Sets up a dynamic play by gathering the products.category into an array. 
+  then, converts it into a set to remove duplicate*/}
 
   return (
     <>
@@ -67,33 +68,30 @@ const HomeScreen = ({ match }) => {
           <label>
             Category
           <select value={selectedCategory} onChange={handleChange}>
-          
-            {categories.map((category) => (
+
+            {/* Dynmically iterate through the categ */}
+            {categories.map((category) => ( 
               <option key={category} value={category}>
+                {/* Iterates through the dynamic filtered set of products. Sets up the  options to be of those iterators*/}
                 {category}
               </option>
             ))}
           </select>
 
           <form>
-            <input type="number" placeholder="Lower Bound Price" />
-            <input type="number" placeholder="Upper Bound Price" />
-            <button type="submit">Apply Filter</button>
+            <input type="number" placeholder="Lower Bound Price" /> {/* Gathers information and processes into sendable variables */}
+            <input type="number" placeholder="Upper Bound Price" /> {/* Processes the input information into a variable that can be sent to API / backend function*/}
+            <button type="submit">Apply Filter</button>             {/* Button for when user is ready to search*/}
           </form>
         </label>
-
         </div>
-
-        {/* <Button  className='btn-block' type='button'> category </Button> } */}
-
-
       </div>
 
       <>
         <Row>
           {products.map((product) => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
+              <Product product={product} /> 
             </Col>
           ))}
         </Row>
