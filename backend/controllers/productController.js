4083eafb64ba2e0b3467f6f4390ce659ea877c5d
Govind <you@example.com>
2024-01-15 21:53:02 +0530

    MongoError: user is not allowed to do action [remove]

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -46,7 +46,7 @@ const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id)
 
   if (product) {
-    await product.remove()
+    await product.deleteOne()
     res.json({ message: 'Product removed' })
   } else {
     res.status(404)
